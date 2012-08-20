f9131d3e57cf44af22ed6647e3023577d336ded8
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2012-08-20 14:40:11 +0200

    Removed dependencies with 'remote' helpers from Prototype library

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 if Rails.version.to_f >= 3.0
   match '/spent_time/update_project_issues', :controller => 'spent_time', :action => 'update_project_issues'
-  match '/spent_time/create_entry', :controller => 'spent_time', :action => 'create_entry'
+  match '/spent_time/create_entry' => 'spent_time#create_entry', :as => 'create_entry_spent_time'
   match '/spent_time/destroy_entry', :controller => 'spent_time', :action => 'destroy_entry'  
-  match '/spent_time/report', :controller => 'spent_time', :action => 'report'  
+  match '/spent_time/report' => 'spent_time#report', :as => 'report_spent_time'  
   match '/spent_time', :controller => 'spent_time', :action => 'index'
 else
   ActionController::Routing::Routes.draw do |map|
